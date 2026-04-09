9bcb7f8e1fd176dd4966e1435132e44ecd58d8a4
Ricardo Metral <ricardometral2005@gmail.com>
2026-04-09 15:10:25 -0400

    Added launch files for lichess and stt

---
+++ src/chess_common_py/launch/lichess_chess.launch.py
@@ -0,0 +1,30 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import TimerAction
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='chess_board_state',
+            executable='chess_board_state_node',
+            output='screen',
+            arguments=['--ros-args', '--log-level', 'info'] 
+        ),
+        TimerAction(
+            period=3.0,
+            actions=[
+                Node(
+                    package='chess_player_input',
+                    executable='lichess_player_input_node',
+                    output='screen',
+                    arguments=['--ros-args', '--log-level', 'info'] 
+                ),
+                Node(
+                    package='chess_planner',
+                    executable='chess_planner_node',
+                    output='screen',
+                    arguments=['--ros-args', '--log-level', 'info'] 
+                ),
+            ]
+        ),
+    ])
